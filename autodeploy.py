5768accc8071a1d271052878469883308a33dc48
Fenhl <fenhl@fenhl.net>
2016-05-03 21:08:01 +0000

    Import gitdir and remove host-specific logic

--- autodeploy.py
+++ autodeploy.py
@@ -6,6 +6,7 @@ gitdir autodeploy
 
 import bottle
 import datetime
+import gitdir.repo
 import json
 import os.path
 import subprocess
@@ -41,72 +42,30 @@ def try_subprocess(args, cwd=None, request_time=None):
                 print(popen.stdout.read().decode('utf-8'), file=log_file)
         raise subprocess.CalledProcessError(popen.returncode, args)
 
-def github_deploy(user, repo, branch='master', request_time=None):
+@application.route('/deploy/<hostname>/<repo_spec:path>/<branch>')
+def deploy(hostname, repo_spec, branch='master', request_time=None):
     try:
-        if branch == 'master' or branch is None:
-            cwd = os.path.join('/opt/git/github.com', user, repo, 'master')
-        else:
-            cwd = os.path.join('/opt/git/github.com', user, repo, 'branch', branch)
-        try_subprocess(['git', 'fetch', 'origin'], cwd=cwd, request_time=request_time)
-        try_subprocess(['git', 'reset', '--hard', 'origin/{}'.format(branch or 'master')], cwd=cwd, request_time=request_time) #TODO don't reset gitignored files (or try merging and reset only if that fails)
+        gitdir.repo.by_name(hostname).deploy(repo_spec, branch=branch)
     except Exception as e:
         if 'logPath' in config and os.path.exists(config['logPath']) and request_time is not None:
             with open(os.path.join(config['logPath'], request_time.strftime('%Y%m%d-%H%M%S-%f-error.log')), 'a') as log_file:
-                print('Error while deploying from github.com:', file=log_file)
-                print('username: ' + user, file=log_file)
-                print('repo: ' + repo, file=log_file)
+                print('Error while deploying from {}:'.format(hostname), file=log_file)                print('username: ' + user, file=log_file)
+                print('repo: ' + repo_spec, file=log_file)
                 print('branch: ' + (branch or 'master'), file=log_file)
                 traceback.print_exc(file=log_file)
-        raise DeployError('{} while deploying. host: github.com, user: {!r}, repo: {!r}, branch: {!r}'.format(e.__class__.__name__, user, repo, branch)) from e
+        raise DeployError('{} while deploying. host: {!r}, user: {!r}, repo: {!r}, branch: {!r}'.format(e.__class__.__name__, hostname, repo_spec, branch)) from e
 
 @application.route('/')
 def show_index():
     return bottle.static_file('static/index.html', root=config.get('documentRoot', os.path.dirname(os.path.abspath(__file__))))
 
-@application.route('/deploy/<host>/<repo:path>')
-def deploy_repo(host, repo):
-    if host == 'github.com':
-        repo_parts = repo.split('/')
-        if len(repo_parts) > 3:
-            raise ValueError('{!r} is an invalid repository specifier for github.com'.format(repo))
-        user = repo_parts[0]
-        repo = repo_parts[1]
-        branch = repo_parts[2] if len(repo_parts) >= 3 else 'master'
-        github_deploy(user, repo, branch=branch)
-    else:
-        raise ValueError('Unsupported host: {!r}'.format(host))
-
 @application.route('/deploy')
 def get_deploy():
     request_time = datetime.datetime.utcnow()
     for host, host_data in config.get('repos', {}).items():
-        if host == 'github.com':
-            for user, repo_data in host_data.items():
-                for repo, branches in repo_data.items():
-                    for branch in branches:
-                        github_deploy(user, repo, branch=branch, request_time=request_time)
-        elif host == 'gitlab.com':
-            for user, repo_data in host_data.items():
-                for repo, branches in repo_data.items():
-                    for branch in branches:
-                        try:
-                            if branch == 'master' or branch is None:
-                                cwd = os.path.join('/opt/git/gitlab.com', user, repo, 'master')
-                            else:
-                                cwd = os.path.join('/opt/git/gitlab.com', user, repo, 'branch', branch)
-                            try_subprocess(['git', 'fetch', 'readonly'], cwd=cwd, request_time=request_time)
-                            try_subprocess(['git', 'reset', '--hard', 'readonly/{}'.format(branch or 'master')], cwd=cwd, request_time=request_time) #TODO don't reset gitignored files (or try merging and reset only if that fails)
-                        except Exception as e:
-                            if 'logPath' in config and os.path.exists(config['logPath']):
-                                with open(os.path.join(config['logPath'], request_time.strftime('%Y%m%d-%H%M%S-%f-error.log')), 'a') as log_file:
-                                    print('Error while deploying from gitlab.com:', file=log_file)
-                                    print('username: ' + user, file=log_file)
-                                    print('repo: ' + repo, file=log_file)
-                                    print('branch: ' + (branch or 'master'), file=log_file)
-                                    traceback.print_exc(file=log_file)
-                            raise
-        else:
-            raise LookupError('host not currently supported: {!r}'.format(host))
+        for repo_spec, branches in host_data.items():
+            for branch in branches:
+                deploy(host, repo_spec, branch=branch, request_time=request_time)
 
 @application.route('/deploy', method='POST')
 def post_deploy():
